71134ab98f4f5abbb8a2aba7556bdc19ef0d0633
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-24 06:53:12 -0300

    feat: adding middleware

--- src/index.js
+++ src/index.js
@@ -12,6 +12,21 @@ const customers = [];
 // id - uuid
 // statement []
 
+// Middleware
+function VerifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf); 
+    
+    if(!customer) {
+      return response.status(400).json({error: "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -31,16 +46,12 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 })
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({error: "Customer not found"});
-  }
-
+app.get("/statement", VerifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  
   return response.json(customer.statement)
 })
 
 app.listen(3333);
+
+// app.use(VerifyIfExistsAccountCPF)
